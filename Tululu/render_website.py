f05d2d8bf49cee9aa489111b6d3692b039ad9cf8
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-06-04 13:11:56 +0300

    Add livereload

--- Tululu/render_website.py
+++ Tululu/render_website.py
@@ -1,9 +1,17 @@
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
 
 
 def main():
+    render_index_page()
+    server = Server()
+    server.watch('template.html', render_index_page)
+    server.serve(port=5501, root='.')
+
+
+def render_index_page():
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
